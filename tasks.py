0e53dc55bc7ccc79f2ab0c6f3c932ef3b6c1a01c
Nick Shaw <nick@nicks-mac-pro.home>
2019-03-13 18:24:43 +0000

    Add equations and images to Appendix 4.1.

--- tasks.py
+++ tasks.py
@@ -433,6 +433,6 @@ def gh_deploy(ctx):
     result = ctx.run('git rev-parse HEAD', hide='both')
     sha = result.stdout.strip().split('\n')[0]
     ctx.run('ghp-import '
-            '-c "cinematiccolor.org" '
+#            '-c "cinematiccolor.org" '
             '-m "Deploy {0} with \"ghp-import\"." '
             '-p {1}'.format(sha, HTML_RELEASE_DIRECTORY))
